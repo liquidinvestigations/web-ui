8a613195aaf1e54578fbc460effecbc3562f4f7e
nic <nicolae.mihale@gmail.com>
2017-10-04 00:15:58 +0300

    Improved admin layout

--- src/app/admin/network/network.component.ts
+++ src/app/admin/network/network.component.ts
@@ -2,31 +2,25 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    templateUrl: './network.component.html'
+    templateUrl: './network.component.html',
+    styleUrls: [
+        './network.component.scss'
+    ]
 })
 export class NetworkComponent {
 
     tabs = [];
 
     constructor(private activatedRoute: ActivatedRoute) {
-
-        console.log(this.activatedRoute);
-
         let routeChildren = this.activatedRoute.routeConfig.children;
 
         if (routeChildren) {
-            let parentPath = activatedRoute.routeConfig.path;
-
             for (let route of routeChildren) {
                 if (route.path) {
                     route.data.path = './' + route.path;
                     this.tabs.push(route.data);
                 }
             }
-        } else {
-            this.tabs = [];
         }
-
-
     }
 }
--- src/app/panel-layout/panel-layout.module.ts
+++ src/app/panel-layout/panel-layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SideMenuComponent } from './sidebar/side-menu.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     imports: [
@@ -9,10 +10,12 @@ import { SideMenuComponent } from './sidebar/side-menu.component';
         RouterModule,
     ],
     declarations: [
-        SideMenuComponent
+        SideMenuComponent,
+        NavbarComponent
     ],
     exports: [
         SideMenuComponent,
+        NavbarComponent
     ]
 })
 export class PanelLayoutModule {
