9d542e422f98c0d6b91acd8d44b6b32bd417f793
nic <nicolae.mihale@gmail.com>
2017-10-29 01:15:57 +0300

    Add better UI for general status.

--- src/app/admin/general-status/general-status.component.ts
+++ src/app/admin/general-status/general-status.component.ts
@@ -6,17 +6,71 @@ import { ApiClientService } from '../../core/api-client.service';
     styleUrls: ['./general-status.component.scss']
 })
 export class GeneralStatusComponent {
-    currentConfig: {} = {};
-
-    services: any[] = [];
+    currentConfig: any[] = [];
 
     constructor(protected apiService: ApiClientService) {
-
-        this.apiService.get(
-            'api/network/status/'
-        ).subscribe((statusResponse: any) => {
-            this.currentConfig = statusResponse;
-        });
+        this.refreshStatus();
     }
 
+    refreshStatus() {
+        this.currentConfig = [];
+
+        this.apiService.get('/api/network/status/')
+            .subscribe((response: any) => {
+                for (let i = 0; i < response.interfaces.length; i++) {
+                    this.currentConfig.push(
+                        {
+                            title: 'Interface #' + i,
+                            fields: this.mapStatus(response.interfaces[i])
+                        }
+                    );
+                }
+            });
+    }
+
+    mapStatus(response) {
+        const mapping = {
+            'name': {
+                label: 'Name',
+            },
+            'label': {
+                label: 'Label',
+            },
+            'ip_address': {
+                label: 'IP'
+            },
+            'ssid': {
+                label: 'SSID'
+            },
+            'error_message': {
+                label: 'Error message',
+                valueClass: 'text-danger'
+            }
+        };
+
+        let out = [];
+        for (let prop in response) {
+            if (response.hasOwnProperty(prop) && mapping[prop] && response[prop] !== null) {
+                let iconClass = '';
+                let itemValue = response[prop];
+                let displayValue = response[prop];
+
+                switch (typeof itemValue) {
+                    case 'boolean':
+                        iconClass = response[prop] ? 'fa fa-check text-success' : 'fa fa-times text-danger';
+                        displayValue = '';
+                        break;
+                }
+
+                let item = Object.create(mapping[prop]);
+                item.value = itemValue;
+                item.displayValue = displayValue;
+                item.iconClass = iconClass;
+
+                out.push(item);
+            }
+        }
+
+        return out;
+    }
 }
