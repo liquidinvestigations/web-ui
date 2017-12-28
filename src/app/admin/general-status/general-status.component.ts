76a047b54fc7bf80459f312c2f1bc16fba1fd3fa
m-nic <nicolae.mihale@gmail.com>
2017-12-28 17:00:11 +0200

    refs liquidinvestigations/web-ui#2. Add shutdown and reboot buttons on the general status page.

--- src/app/admin/general-status/general-status.component.ts
+++ src/app/admin/general-status/general-status.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ApiClientService } from '../../core/api-client.service';
+import { BsModalComponent } from '../../shared/bs-modal/bs-modal.component';
 
 @Component({
     templateUrl: './general-status.component.html',
 })
 export class GeneralStatusComponent {
+    @ViewChild('confirmationModal') confirmationModal: BsModalComponent;
+
     currentConfig: {} = {
         title: 'Messages'
+    };
+
+    confirmation: {
+        title: string,
+        body: string,
+        ok: {
+            iconClass: string,
+            buttonClass: string,
+            text: string,
+            action: () => void
+        }
     };
 
     constructor(protected apiService: ApiClientService) {
@@ -24,5 +38,47 @@ export class GeneralStatusComponent {
                         : null
                 };
             });
+    }
+
+    confirmReboot() {
+        this.confirmation = {
+            title: 'Confirm system reboot',
+            body: 'Are you sure you want to restart the system?',
+            ok: {
+                iconClass: 'fa fa-refresh',
+                buttonClass: 'btn btn-warning',
+                text: 'Reboot',
+                action: () => {
+                    this.confirmationModal.hide();
+                    this.shutdown(true);
+                }
+            }
+        };
+
+        this.confirmationModal.show();
+    }
+
+    confirmShutDown() {
+        this.confirmation = {
+            title: 'Confirm system shutdown',
+            body: 'Are you sure you want to shutdown the system?',
+            ok: {
+                iconClass: 'fa fa-power-off',
+                buttonClass: 'btn btn-danger',
+                text: 'Shutdown',
+                action: () => {
+                    this.confirmationModal.hide();
+                    this.shutdown(false);
+                }
+            }
+        };
+
+        this.confirmationModal.show();
+    }
+
+    shutdown(reboot: boolean = false) {
+        this.apiService
+            .post('/api/shutdown/', { action: (reboot ? 'reboot' : 'poweroff') })
+            .subscribe();
     }
 }
